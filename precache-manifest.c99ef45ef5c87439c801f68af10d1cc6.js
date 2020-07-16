38122b882c10011e17c15d4723e48fc4aaca8d99
Etherum7 <hvs.231918@gmail.com>
2020-07-16 20:45:59 +0530

    Updates

---
+++ precache-manifest.c99ef45ef5c87439c801f68af10d1cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6044caedba73b9304b7f13d46ca39507",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a8f1d27f5bc45bf9cf1d",
+    "url": "/monsters-rolodex/static/css/main.c9f98726.chunk.css"
+  },
+  {
+    "revision": "d6ee5c3d57ecb6c75f02",
+    "url": "/monsters-rolodex/static/js/2.e644cc65.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/monsters-rolodex/static/js/2.e644cc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f1d27f5bc45bf9cf1d",
+    "url": "/monsters-rolodex/static/js/main.010780d9.chunk.js"
+  },
+  {
+    "revision": "88f2dd83258b152f7908",
+    "url": "/monsters-rolodex/static/js/runtime-main.ffcc526b.js"
+  }
+]);
